39921cf1ef24896999a7346af9b3f2ae679fd541
neoterux <labfernandez2014@gmail.com>
2022-10-13 18:09:55 -0500

    Created interface InsuranceApplier

---
+++ app/src/main/java/ec/edu/espol/workshops/core/insurance/InsuranceApplier.java
@@ -0,0 +1,27 @@
+package ec.edu.espol.workshops.core.insurance;
+
+import ec.edu.espol.workshops.utils.InsuranceException;
+
+/**
+ * This interface represents an a capable entity to
+ * apply at an insurance.
+ * 
+ * @author neoterux
+ *
+ */
+public interface InsuranceApplier {
+	
+	/**
+	 * The base value of an insurance value
+	 */
+	final int BASE_VALUE = 500;
+	
+	/**
+	 * Calculates the value of the insurance.
+	 * 
+	 * @return The value of the insurance to pay.
+	 * @throws InsuranceException If the applier cannot apply to an insurance
+	 */
+	long calculateInsuranceValue() throws InsuranceException;
+
+}
