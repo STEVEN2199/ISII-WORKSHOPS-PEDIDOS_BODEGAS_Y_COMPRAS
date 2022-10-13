760232e7e2f3491b3004754264d2df8ef71b5dc2
neoterux <labfernandez2014@gmail.com>
2022-10-13 18:09:34 -0500

    Created customer class

---
+++ app/src/main/java/ec/edu/espol/workshops/core/entities/Customer.java
@@ -0,0 +1,16 @@
+package ec.edu.espol.workshops.core.entities;
+
+import ec.edu.espol.workshops.core.insurance.InsuranceApplier;
+import ec.edu.espol.workshops.utils.InsuranceException;
+
+public class Customer implements InsuranceApplier{
+
+	@Override
+	public long calculateInsuranceValue() throws InsuranceException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	
+
+}
