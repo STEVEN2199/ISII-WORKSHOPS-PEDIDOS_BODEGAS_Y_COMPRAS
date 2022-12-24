6f3ca2cc69016adb9ea8655b8c67939169464958
José Jaramillo <josjjara@espol.edu.ec>
2022-12-23 23:03:11 -0500

    Changes to unsused code

--- app/src/main/java/ec/edu/espol/workshops/core/entities/Customer.java
+++ app/src/main/java/ec/edu/espol/workshops/core/entities/Customer.java
@@ -5,11 +5,11 @@ import ec.edu.espol.workshops.utils.InsuranceException;
 
 /**
  * <h1>Customer Class.</h1>
- * 
+ *
  * <p>
  * This class represents a customer entity that is on the project.
  * </p>
- * 
+ *
  *
  *
  * @author neoterux
@@ -25,69 +25,41 @@ public class Customer implements InsuranceApplier {
   public Customer() {
   }
 
-  /**
-   * Create a new Customer object.
-   *
-   * @param age The age of the customer
-   * @param sex The sex of the customer
-   * @param isMarried Is if married or not
-   * @param driverLicenceValid  The valid thru date
-   */
-  public Customer(int age, char sex, boolean isMarried, boolean driverLicenceValid) {
-    this.age = age;
-    this.sex = sex;
-    this.isMarried = isMarried;
-    this.driverLicenceValid = driverLicenceValid;
+  @Override
+  public long calculateInsuranceValue() throws InsuranceException {
+
+    var premiumCost = BASE_VALUE;
+    if (this.sex == 'F' || this.isMarried) {
+      premiumCost -= 200;
+    }
+    if (this.sex == 'M' && !this.isMarried && this.age < 25) {
+      premiumCost += 1500;
+    }
+    if (this.age >= 45 && this.age < 65) {
+      premiumCost -= 100;
+    }
+    return premiumCost;
+
   }
 
   public int getEdad() {
-    return age;
+    return this.age;
   }
 
   public void setEdad(final int age) {
     this.age = age;
   }
 
-  public char getSex(final char sex) {
-    return sex;
-  }
-
-  public void setSex(final char sex) {
-    this.sex = sex;
-  }
-
-  public boolean getMaritalStatus() {
-    return isMarried;
+  public void setLicenceIsValid(final boolean driverLicenceValid) {
+    this.driverLicenceValid = driverLicenceValid;
   }
 
   public void setMaritalStatus(final boolean isMarried) {
     this.isMarried = isMarried;
   }
 
-  public boolean isLicenseValid () {
-    return driverLicenceValid;
-  }
-
-  public void setLicenceIsValid(final boolean driverLicenceValid) {
-    this.driverLicenceValid = driverLicenceValid;
-  }
-
-  @Override
-  public long calculateInsuranceValue() throws InsuranceException {
-    if (age >= 80 || !isLicenseValid() || age < 0) return -1;
-  
-    int premiumCost = BASE_VALUE;
-    if (sex == 'F' || isMarried) {
-      premiumCost -= 200;
-    }
-    if (sex == 'M' && !isMarried && age < 25) {
-      premiumCost += 1500;
-    }
-    if (age >= 45 && age < 65) {
-      premiumCost -= 100;
-    }
-    return premiumCost;
-  
+  public void setSex(final char sex) {
+    this.sex = sex;
   }
 
 }
